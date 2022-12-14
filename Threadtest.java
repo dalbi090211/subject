867de2baaf13667ba2ea98619220b81b2e6e489a
dalbi090211 <trump0911@naver.com>
2022-12-14 14:17:32 +0000

    [sdf]

--- Threadtest.java
+++ Threadtest.java
@@ -15,7 +15,11 @@ class MyThread1 extends Thread {
 }
 //runnable객체를 만들어 스레드를 만드는 방식
 class MyThread2 implements Runnable {
-	
+	int sequence;
+    MyThread2(int sequence){
+        super();
+        this.sequence = sequence;
+    }
     public void run() {
          try{
             System.out.println("3번 스레드 실행입니다.");
@@ -32,8 +36,8 @@ public class Threadtest {
         int thread_dead_point = 0;  
         t1 = new MyThread1(1);
         t2 = new MyThread1(2);
-        Runnable r3 = new MyThread2();
-        t3 = new Thread(r3, "TT");
+        Runnable r3 = new MyThread2(3);
+        t3 = new Thread(r3, "Runnable_Class");
 
         //스레드의 동작순서는 작성순서와는 상관없음
         t1.start();
@@ -41,6 +45,7 @@ public class Threadtest {
         t3.start();
         //isAlive()를 통해 스레드가 죽었는지를 확인
         while(thread_dead_point != 3){
+            thread_dead_point = 0;
             if(!t1.isAlive()){
                 thread_dead_point += 1;
             }
